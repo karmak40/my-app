5c12008f7bb28ece14d6df433d47fca0ce65d2c4
khlush <hlushchenko.kostiantyn@signon-group.com>
2018-01-25 13:43:42 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { InMemoryDataService } from './in-memory-data.service';
 // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
 // and returns simulated server responses.
 // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot (InMemoryDataService, {dataEncapsulation: false})
+   // HttpClientInMemoryWebApiModule.forRoot (InMemoryDataService, {dataEncapsulation: false})
   ],
   providers: [HeroService, MessageService, InMemoryDataService],
   bootstrap: [AppComponent]
